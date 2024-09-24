b734a87273bb1e162f1a2c8706f33461db390206
Julien <0juliendupuis0@gmail.com>
2024-09-24 03:00:07 +0200

    Added LSP and Autocomplete

---
+++ .config/nvim/lua/judup/plugins/lsp-config.lua
@@ -0,0 +1,70 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+
+        config = function()
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('Personal-lsp-attach', { clear = true }),
+                callback = function(event)
+                    local client = vim.lsp.get_client_by_id(event.data.client_id)
+                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+                        local highlight_augroup = vim.api.nvim_create_augroup('Personal-lsp-highlight', { clear = false })
+
+                        vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                            buffer = event.buf,
+                            group = highlight_augroup,
+                            callback = vim.lsp.buf.clear_references,
+                        })
+
+                        vim.api.nvim_create_autocmd('LspDetach', { 
+                            group = vim.api.nvim_create_augroup('Personal-lsp-detach', { clear = true }),
+                            callback = function(event2)
+                                vim.lsp.buf.clear_references()
+                                vim.api.nvim_clear_autocmd { group = 'Personal-lsp-highlight', buffer = event2.buf }
+                            end,
+                        })
+                    end
+                end
+            })
+
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+            local servers = {
+                        lua_ls = {},
+                        cmake = {},
+                        harper_ls = {},
+                    }
+            
+            require('mason').setup()
+
+            local ensured_installed = vim.tbl_keys(servers or {})
+            vim.list_extend(ensure_installed or {}, { 'stylua', })
+
+            require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+            require('mason-lspconfig').setup {
+                handlers = {
+                    function(server_name)
+                        local server = servers[server_name] or {}
+                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                        require('lspconfig')[server_name].setup(server)
+                    end,
+                },
+            }
+        end
+    },
+}
