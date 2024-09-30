8f3152f2d817b4fa20da20e465a8c74c14725eb1
Julien <0juliendupuis0@gmail.com>
2024-09-30 22:32:48 +0200

    Added some remap + Vimtex

--- .config/nvim/lua/judup/plugins/treesitter.lua
+++ .config/nvim/lua/judup/plugins/treesitter.lua
@@ -26,9 +26,9 @@ return {
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = { 'ruby', 'tex' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', 'tex' } },
     },
 },
 
--- .config/nvim/lua/judup/remap.lua
+++ .config/nvim/lua/judup/remap.lua
@@ -1,8 +1,10 @@
 
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>h", vim.cmd.tabprev)
 vim.keymap.set("n", "<leader>l", vim.cmd.tabnext)
 vim.keymap.set("n", "<leader>x", vim.cmd.tabc)
+
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "$")
