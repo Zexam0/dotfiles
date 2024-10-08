ffe307efe9e4e75bca62bd0862febd8454c0bdcc
Zexam0 <0juliendupuis0@gmail.com>
2024-10-08 14:51:16 +0200

    Added Visual Move remap

--- .config/nvim/lua/judup/remap.lua
+++ .config/nvim/lua/judup/remap.lua
@@ -8,3 +8,6 @@ vim.keymap.set("n", "<leader>x", vim.cmd.tabc)
 
 vim.keymap.set("n", "H", "^")
 vim.keymap.set("n", "L", "$")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
