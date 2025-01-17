59d4dae91ef17bb6dd75418544fcbde5196b30a7
Zexam0 <0juliendupuis0@gmail.com>
2025-01-17 15:23:46 +0100

    Tweaked c filetype config

--- .config/nvim/lua/judup/remap.lua
+++ .config/nvim/lua/judup/remap.lua
@@ -20,6 +20,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Matching more characters with i and a option
 local chars = { '_', '.', ':', ',', ';', '<bar>', '/', '<bslash>', '*', '+', '%' }
 
 for _, char in ipairs(chars) do
