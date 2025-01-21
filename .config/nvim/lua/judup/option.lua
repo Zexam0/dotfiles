098f0d7d44bf1c7eae8afabfc416e6ea32f14509
Zexam0 <0juliendupuis0@gmail.com>
2025-01-21 19:05:34 +0100

    Deleted unused plugin, Reformatted code and better whitespace characters

--- .config/nvim/lua/judup/option.lua
+++ .config/nvim/lua/judup/option.lua
@@ -29,7 +29,7 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = '| ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = "tab:▏ ,trail:·,leadmultispace:▏ ,nbsp:␣"
 
 vim.opt.inccommand = 'split'
 
--- .config/nvim/lua/judup/plugins/lsp-config.lua
+++ .config/nvim/lua/judup/plugins/lsp-config.lua
@@ -1,129 +1,128 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("Personal-lsp-attach", { clear = true }),
-				callback = function(event)
-					-- Format Text using lsp
-					local map = function(keys, func, desc, mode)
-						mode = mode or "n"
-						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("Personal-lsp-attach", { clear = true }),
+        callback = function(event)
+          -- Format Text using lsp
+          local map = function(keys, func, desc, mode)
+            mode = mode or "n"
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
 
-					map("gf", vim.lsp.buf.format, "Format file")
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          map("gf", vim.lsp.buf.format, "Format file")
+          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-						local highlight_augroup =
-							vim.api.nvim_create_augroup("Personal-lsp-highlight", { clear = false })
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+            local highlight_augroup =
+                vim.api.nvim_create_augroup("Personal-lsp-highlight", { clear = false })
 
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
+            })
 
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
 
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("Personal-lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({ group = "Personal-lsp-highlight", buffer = event2.buf })
-							end,
-						})
-					end
-				end,
-			})
+            vim.api.nvim_create_autocmd("LspDetach", {
+              group = vim.api.nvim_create_augroup("Personal-lsp-detach", { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds({ group = "Personal-lsp-highlight", buffer = event2.buf })
+              end,
+            })
+          end
+        end,
+      })
 
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			local servers = {
-				lua_ls = {
-					on_init = function(client)
-						if client.workspace_folders then
-							local path = client.workspace_folders[1].name
-							if vim.loop.fs_stat(path .. "/luarc.json") or vim.loop.fs_stat(path .. "/luarc.jsonc") then
-								return
-							end
-						end
+      local servers = {
+        lua_ls = {
+          on_init = function(client)
+            if client.workspace_folders then
+              local path = client.workspace_folders[1].name
+              if vim.loop.fs_stat(path .. "/luarc.json") or vim.loop.fs_stat(path .. "/luarc.jsonc") then
+                return
+              end
+            end
 
-						client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-							runtime = {
-								version = "LuaJIT",
-							},
+            client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+              runtime = {
+                version = "LuaJIT",
+              },
 
-							workspace = {
-								checkThirdParty = false,
-								library = {
-									vim.env.VIMRUNTIME,
-									"${3rd}/luv/library",
-								},
-							},
-						})
-					end,
-					settings = {
-						Lua = {
-							diagnostics = {
-								disable = {
-									"spell-check",
-									"trailing-space",
-									"lowercase-global",
-								},
-							},
-							hint = {
-								enable = false,
-							},
-						},
-					},
-				},
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                },
+              },
+            })
+          end,
+          settings = {
+            Lua = {
+              diagnostics = {
+                disable = {
+                  "spell-check",
+                  "trailing-space",
+                  "lowercase-global",
+                },
+              },
+              hint = {
+                enable = false,
+              },
+            },
+          },
+        },
 
-				cmake = {},
-				clangd = {
-					cmd = {
-						"clangd",
-						"--fallback-style=WebKit",
-					},
-				},
-			}
+        cmake = {},
+        clangd = {
+          cmd = {
+            "clangd",
+            "--fallback-style=Google",
+          },
+        },
+      }
 
-			require("mason").setup()
+      require("mason").setup()
 
-			local ensured_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensured_installed or {}, { "stylua" })
+      local ensured_installed = vim.tbl_keys(servers or {})
+      vim.list_extend(ensured_installed or {}, { "stylua" })
 
-			require("mason-tool-installer").setup({ ensure_installed = ensured_installed })
+      require("mason-tool-installer").setup({ ensure_installed = ensured_installed })
 
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
-			})
-		end,
-	},
+      require("mason-lspconfig").setup({
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            require("lspconfig")[server_name].setup(server)
+          end,
+        },
+      })
+    end,
+  },
 }
