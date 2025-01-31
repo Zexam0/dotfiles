9e4d791545cd2263d5c1846a429564c7eab43f48
Zexam0 <0juliendupuis0@gmail.com>
2025-01-31 12:29:08 +0100

    Added correct filetype settings for y86 files

--- .config/nvim/lua/judup/option.lua
+++ .config/nvim/lua/judup/option.lua
@@ -7,19 +7,20 @@ vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
 
+-- Correct indentation
 vim.opt.breakindent = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Remove annoying files
 vim.opt.undofile = true
 vim.opt.swapfile = false
 
+-- QoL
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
-vim.opt.signcolumn = 'no'
 
 vim.opt.updatetime = 50
 vim.opt.timeoutlen = 500
@@ -30,12 +31,23 @@ vim.opt.splitbelow = true
 
 vim.opt.list = true
 vim.opt.listchars = "tab:▏ ,trail:·,leadmultispace:▏ ,nbsp:␣"
+vim.opt.cursorline = true
 
 vim.opt.inccommand = 'split'
-
-vim.opt.cursorline = true
 
 vim.opt.scrolloff = 8
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+
+-- We Hate signcolumn in this house
+vim.opt.signcolumn = 'no'
+
+-- Correct filetype for y86
+vim.api.nvim_create_autocmd({"BufNewFile","BufRead"}, {
+  pattern = {"*.ys"},
+  callback = function() vim.opt.filetype = "asm" end,
+})
+
+
+
