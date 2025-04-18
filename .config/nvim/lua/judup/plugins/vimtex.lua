6989bc6394f1ffb674d1d4e0551a967f99448e40
Zexam0 <0juliendupuis0@gmail.com>
2025-04-18 13:00:49 +0200

    Vimtex Quick Recompile and show mapping

--- .config/nvim/lua/judup/plugins/vimtex.lua
+++ .config/nvim/lua/judup/plugins/vimtex.lua
@@ -8,8 +8,15 @@ return {
       vim.g.vimtex_mappings_enabled = 0
       vim.g.vimtex_format_enabled = 1
       vim.g.vimtex_format_method = "latexindent"
+      vim.g.vimtex_quickfix_enable = 0 
+      vim.g.vimtex_quickfix_mode = 0
       vim.keymap.set("n", "<leader>cc", "<plug>(vimtex-compile-ss)")
       vim.keymap.set("n", "<leader>cv", "<plug>(vimtex-view)")
+      vim.keymap.set("n", "<leader>cr", function()
+        vim.cmd("silent! write")
+        vim.fn["vimtex#compiler#compile_ss"]()
+        vim.fn["vimtex#view#view"]()
+      end)
       vim.api.nvim_create_autocmd({"User"},{
           pattern = "VimtexEventQuit",
           command = "VimtexClean",
