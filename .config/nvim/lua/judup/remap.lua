fa4ee3c3cc29f67617cba5bd9e1bc0d5dc57975f
Zexam0 <0juliendupuis0@gmail.com>
2025-07-14 15:56:35 +0200

    Mapping for section lines

--- .config/nvim/lua/judup/remap.lua
+++ .config/nvim/lua/judup/remap.lua
@@ -13,13 +13,13 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 vim.keymap.set("n", "H", "^")
 vim.keymap.set("n", "L", "$")
 
--- Window Switch 
+-- Window Switch like tmux
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Greatest Keymap ever
+-- (Second) Greatest Keymap ever
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -31,4 +31,28 @@ for _, char in ipairs(chars) do
   vim.api.nvim_set_keymap('o', 'i' .. char, ':normal vi' .. char .. '<CR>', { noremap = true, silent = true })
   vim.api.nvim_set_keymap('x', 'a' .. char, ':<C-u>normal! F' .. char .. 'vf' .. char .. '<CR>', { noremap = true, silent = true })
   vim.api.nvim_set_keymap('o', 'a' .. char, ':normal va' .. char .. '<CR>', { noremap = true, silent = true })
-end
+end 
+
+-- Create Section lines
+vim.keymap.set("n", "<leader>S", function()
+  local comment_str = vim.bo.commentstring or "// %s"
+  local comment_prefix = comment_str:match("^([^%%]+)") or "//"
+  -- ^ : Anchor to beginning 
+  -- ( ) : Capture this group
+  -- [^%%] : Any char not equal to % (double it to escape it) 
+  -- + : one or more
+  comment_prefix = comment_prefix:gsub("%s*$", "")
+  -- %s : any whitespace
+  -- * : zore or more
+  -- $ : end of string
+  -- gsub : replace the match with the second string
+
+  local section_line = string.format("%s%s",
+    comment_prefix,
+    string.rep(comment_prefix, 20)
+  )
+  -- string.format( [string format], s1, s2 ... sn) 
+
+  vim.api.nvim_put({section_line}, "l", true, true)
+end)
+
