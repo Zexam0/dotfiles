b125cbb7bf12c91f8ca02fc157a81ba0df823665
Zexam0 <0juliendupuis0@gmail.com>
2025-08-09 17:00:04 +0200

    Cht.sh and personnal postit integration with tmux

---
+++ bin/scripts/chtsh_postit.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+languages=`echo "nodejs bash c cpp" | tr ' ' '\n'`
+postit=`echo "regex ANSI-esc-seq" | tr ' ' '\n'`
+core_utils=`echo "xargs chmod mv sed awk mkdir" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$postit\n$core_utils" | fzf`
+
+if printf $postit | grep -qs $selected; then
+    less -R "$HOME/dotfiles/postit/${selected}.txt"
+    exit 0
+fi 
+
+read -p "query: " query
+
+if printf $languages | grep -qs $selected; then
+    curl cht.sh/$selected/`echo $query | tr ' ' '+'` | less
+else
+    curl cht.sh/$selected~$query | less
+fi
+
