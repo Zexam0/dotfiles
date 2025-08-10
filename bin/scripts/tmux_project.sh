dfafccef7666fd01d3d85d4e54b4a1b84da41e41
Zexam0 <0juliendupuis0@gmail.com>
2025-08-10 12:04:26 +0200

    Binding new tmux shortcuts

--- bin/scripts/tmux_project.sh
+++ bin/scripts/tmux_project.sh
@@ -7,21 +7,32 @@ set -e
 
 source $HOME/.bash_functions
 
-function tmux_switch_client ()
-{
+function _handle_select () {
+    local action=$1
     launch_selection
-    if ! tmux has-session -t="$name" 2>/dev/null; then
-        tmux new-session -ds "$name" -c "$selected" 
-    fi
 
-    tmux switch-client -t "$name"
+    if [[ -z "$selected" ]]; then
+        tmux display-message "Operation Aborted: Nothing selected!"
+        return
+    fi 
+
+    case "$action" in
+        switch) 
+            if ! tmux has-session -t="$name" 2>/dev/null; then
+                tmux new-session -ds "$name" -c "$selected" 
+            fi
+            tmux switch-client -t "$name"
+            ;;
+        window)
+            tmux new-window -n "$name" -c "$selected"
+            ;;
+        *)
+            tmux display-message "Unknown action: $action"
+            ;;
+    esac
 }
 
-function tmux_open_choosen_window ()
-{
-    launch_selection
-    tmux new-window -n "$name" -c "$selected"
-    echo "$name" "$selected"
-}
+function tmux_switch_client () { _handle_select switch; }
+function tmux_open_choosen_window () { _handle_select window; }
 
 "$@"
